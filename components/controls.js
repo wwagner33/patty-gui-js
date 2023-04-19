daf2289ee72d6aa30eeedbadf9776e902c7e2ccc
Wellington Wagner Ferreira Sarmento <wwagner@virtual.ufc.br>
2023-04-18 21:48:57 -0300

    Update controls.js

--- components/controls.js
+++ components/controls.js
@@ -1,29 +1,30 @@
-// controls.js
-let idCounter = 0;
+class Controls {
+  static idCounter = 0;
 
-function createUniqueId() {
-  idCounter++;
-  return 'control_' + idCounter;
+  static createUniqueId() {
+    this.idCounter++;
+    return 'control_' + this.idCounter;
+  }
+
+  static createButton(text, clickCallback) {
+    let button = document.createElement('button');
+    button.textContent = text;
+    button.addEventListener('click', clickCallback);
+    return button;
+  }
+
+  static createLabel(text) {
+    let label = document.createElement('label');
+    label.textContent = text;
+    return label;
+  }
+
+  static createTextbox(defaultValue) {
+    let textbox = document.createElement('input');
+    textbox.type = 'text';
+    textbox.value = defaultValue || '';
+    return textbox;
+  }
 }
 
-function createButton(text, clickCallback) {
-  let button = document.createElement('button');
-  button.textContent = text;
-  button.addEventListener('click', clickCallback);
-  return button;
-}
-
-function createLabel(text) {
-  let label = document.createElement('label');
-  label.textContent = text;
-  return label;
-}
-
-function createTextbox(defaultValue) {
-  let textbox = document.createElement('input');
-  textbox.type = 'text';
-  textbox.value = defaultValue || '';
-  return textbox;
-}
-
-export { createButton, createLabel, createTextbox };
+export { Controls };
